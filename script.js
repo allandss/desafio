af68b5ccafc5633bde3d0d0e2dad04c8a56f909c
Allan Santos <allan.silva@mutant.com.br>
2023-11-21 13:46:17 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMode() {
+  const html = document.documentElement
+  
+  if (html.classList.contains('ligth')) {
+    html.classList.remove('ligth')
+  } else {
+    html.classList.add('ligth')
+  }
+}
